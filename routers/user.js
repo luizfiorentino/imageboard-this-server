6c9dcc0d197d906e4833715aceabb68062753efe
luizfiorentino <lgfiorentino@gmail.com>
2022-04-28 21:50:47 +0200

    hash password added via bcrypt

--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { Route } = require("express");
 const User = require("../models").user;
+const bcrypt = require("bcrypt");
 
 const router = new Router();
 
@@ -12,9 +13,20 @@ router.get("/", async (req, res, next) => {
 
 // Create a new user
 router.post("/", async (req, res, next) => {
-  // const { email, password, fullName } = req.body;
-  const newUser = await User.create(req.body);
-  res.send(newUser);
+  const { email, password, fullName } = req.body;
+  if (!email || !password || !fullName) {
+    res
+      .status(400)
+      .send({ message: "email, password and fullName must be provided" });
+  } else {
+    const newUser = await User.create({
+      email,
+      // handling down the password to the create method we has it
+      password: bcrypt.hashSync(password, 10),
+      fullName,
+    });
+    res.send(newUser);
+  }
 });
 
 // Also works
